3bb5b46bb7625e316c964a8bf0ca95ed68f6550b
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-05-19 11:22:35 +0200

    refactor: make offer script [wip]

--- scripts/make-offer.ts
+++ scripts/make-offer.ts
@@ -10,23 +10,37 @@ import { BN } from 'bn.js';
 
 import { getBossAccount, initProgram, PROGRAM_ID, RPC_URL } from './script-commons';
 
-const SELL_TOKEN_MINT = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'); // USDC Mint Address
-const BUY_TOKEN_MINT = new PublicKey('5Y8NV33Vv7WbnLfq3zBcKSdYPrk7g2KoiQoe7M2tcxp5');  // ONe Mint Address
+// PROD
+// const SELL_TOKEN_MINT = new PublicKey('EPjFWdd6AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'); // USDC Mint Address
+// const BUY_TOKEN_MINT = new PublicKey('5Y8NV33Vv7WbnLfq3zBcKSdYPrk7g2KoiQoe7M2tcxp5');  // ONe Mint Address
+
+// Test & local
+const SELL_TOKEN_MINT = new PublicKey('mntjuZHsfoxSWbjMi7GMrrR5QhA9JaXhaUeyzmqHe3J');  // BSN Mint Address
+const BUY_TOKEN_MINT = new PublicKey('oneRcXEajZ1biQ5Ts9vPHXZ8penh9rRuEBNpx2DA9wE');  //  sUSDe Mint Address 
 
 async function createMakeOfferOneTransaction() {
     const program = await initProgram();
     const connection = new anchor.web3.Connection(RPC_URL);
 
     const offerId = new BN(1);
-    const buyTokenAmount = new BN(200000e9);  // 9 decimals for ONe
-    const sellTokenAmount = new BN(20152574e4); // 6 decimals for USDC
+    const buyTokenAmount = new BN(350000e9);  // 9 decimals for ONe
+    const sellTokenStartAmount = new BN(350000e9); // 6 decimals for USDC
+    const sellTokenEndAmount = new BN(700000e9); // 6 decimals for USDC
+    const offerStartTime = Date.now() / 1000;
+    const offerEndTime = offerStartTime + 7200;
+    const priceFixDuration = new BN(3600);
+
 
     const [offerAuthority] = PublicKey.findProgramAddressSync(
         [Buffer.from('offer_authority'), offerId.toArrayLike(Buffer, 'le', 8)],
         program.programId,
     );
 
+    console.log('programId:', program.programId.toBase58());    
+    console.log('offerAuthority:', offerAuthority.toBase58());
+
     const BOSS = await getBossAccount(program);
+    console.log('BOSS:', BOSS.toBase58());
 
     // TODO: check if boss sell token account exists, create if it doesn't
     const offerSellTokenAccountInstruction = createAssociatedTokenAccountInstruction(
@@ -41,6 +55,7 @@ async function createMakeOfferOneTransaction() {
         getAssociatedTokenAddressSync(BUY_TOKEN_MINT, offerAuthority, true),
         offerAuthority,
         BUY_TOKEN_MINT,
+        TOKEN_PROGRAM_ID
     );
     // Derive the state PDA
     const [statePda] = PublicKey.findProgramAddressSync([Buffer.from('state')], PROGRAM_ID);
@@ -51,13 +66,20 @@ async function createMakeOfferOneTransaction() {
     );
     try {
         const tx = await program.methods
-            .makeOfferOne(offerId, buyTokenAmount, sellTokenAmount)
-            .accountsPartial({
+            .makeOfferOne(
+                offerId,
+                buyTokenAmount,
+                sellTokenStartAmount,
+                sellTokenEndAmount,
+                new BN(offerStartTime),
+                new BN(offerEndTime),
+                priceFixDuration
+            ).accountsPartial({
                 offer: offerPda,
                 offerSellTokenAccount: getAssociatedTokenAddressSync(SELL_TOKEN_MINT, offerAuthority, true),
                 offerBuyToken1Account: getAssociatedTokenAddressSync(BUY_TOKEN_MINT, offerAuthority, true),
                 offerTokenAuthority: offerAuthority,
-                bossBuyToken1Account: getAssociatedTokenAddressSync(BUY_TOKEN_MINT, BOSS, true),
+                bossBuyToken1Account: getAssociatedTokenAddressSync(BUY_TOKEN_MINT, BOSS, false),
                 sellTokenMint: SELL_TOKEN_MINT,
                 buyToken1Mint: BUY_TOKEN_MINT,
                 state: statePda,
