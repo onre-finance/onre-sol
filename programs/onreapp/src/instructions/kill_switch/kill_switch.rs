fcda74d67438a07997e26a78f524bb82dafdc1c6
Marian-Daniel Rolník <marian-danielrolnik@Marian-Daniels-MacBook-Pro.local>
2025-09-19 13:19:35 +0200

    f/ONR-70 - Fix unused variables

--- programs/onreapp/src/instructions/kill_switch/kill_switch.rs
+++ programs/onreapp/src/instructions/kill_switch/kill_switch.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
 
+use crate::constants::seeds;
 use crate::kill_switch_state::KillSwitchState;
 use crate::state::{AdminState, State};
-use crate::constants::seeds;
 
 #[derive(Accounts)]
 pub struct KillSwitch<'info> {
@@ -25,28 +25,25 @@ pub struct KillSwitch<'info> {
     pub signer: Signer<'info>,
 }
 
-
 pub fn kill_switch(ctx: Context<KillSwitch>, enable: bool) -> Result<()> {
     let state = &ctx.accounts.state;
     let signer = &ctx.accounts.signer;
     let admin_state = &ctx.accounts.admin_state;
     let kill_switch_state = &mut ctx.accounts.kill_switch_state;
 
-    // Who actually signed this tx?
     let boss_signed = state.boss.key() == signer.key() && signer.is_signer;
     let admin_signed = admin_state.admins.contains(signer.key) && signer.is_signer;
 
     if enable {
         require!(boss_signed || admin_signed, ErrorCode::UnauthorizedToEnable);
-        ctx.accounts.kill_switch_state.is_killed = true;
+        kill_switch_state.is_killed = true;
     } else {
         require!(boss_signed, ErrorCode::OnlyBossCanDisable);
-        ctx.accounts.kill_switch_state.is_killed = false;
+        kill_switch_state.is_killed = false;
     }
-    
+
     Ok(())
 }
-
 
 #[error_code]
 pub enum ErrorCode {
